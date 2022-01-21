0a0b561688ebdf04cb757660da5399d40d33881d
sshnuke333 <sshnuke@protonmail.com>
2022-01-21 19:25:45 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Calculator
+
+Part of FreeCodeCamp's Front End Development Libraries Project challenge built
+using React, styled-components and material UI.
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Calculator',
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+            { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+        ],
+    },
+    resolve: {
+        alias: {
+            '@mui/styled-engine': '@mui/styled-engine-sc',
+        },
+    },
+};
